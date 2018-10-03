9cb594f75660fdf73a6291c112bc9e94256e976c
Ger Hobbelt <ger@hobbelt.com>
2018-10-03 16:35:40 +0200

    removed coding error in braket extension and regenerated it. Also moved the original CTAN sources to the /unpacked/ subdirectory, where they are more appropriate.

--- legacy/braket/unpacked/braket.js
+++ legacy/braket/unpacked/braket.js
@@ -1,21 +1,3 @@
-/*
- *  ../../../../legacy/braket/braket.js
- *
- *  Copyright (c) 2009-2018 The MathJax Consortium
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 /* -*- Mode: Javascript; indent-tabs-mode:nil; js-indent-level: 2 -*- */
 /* vim: set ts=2 et sw=2 tw=80: */
 
@@ -67,5 +49,4 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready", function() {
   MathJax.Hub.Startup.signal.Post("TeX braket Ready"); 
 });
 
-MathJax.Ajax.loadComplete("[MathJax]/extensions/TeX/braket/braket.js");
-
+MathJax.Ajax.loadComplete("[MathJax]/extensions/TeX/braket/unpacked/braket.js");
