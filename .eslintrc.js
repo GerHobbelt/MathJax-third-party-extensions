4f6fa418420f4d02ad26ac4e366c4515048d954b
Ger Hobbelt <ger@hobbelt.com>
2018-10-03 15:30:11 +0200

    had forgotten to add the eslint config to the repo; also added a `npm run clean` task to clean/remove build files and to make sure the next `npm test` / `npm run build` will produce a fresh and up-to-date set of library files.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,29 @@
 module.exports = {
-	"root": true,
+    "root": true,
     "extends": "eslint:recommended",
-	"env": { 
-		"es6": false,
-		"browser": true,
-		"node": true, 
-	}, 
-	"parserOptions": { 
-		"ecmaVersion": 5,
-		"sourceType": "script",
-		"impliedStrict": true, 
-	},
-	"globals": {
+    "env": { 
+        "es6": false,
+        "browser": true,
+        "node": true, 
+    }, 
+    "parserOptions": { 
+        "ecmaVersion": 5,
+        "sourceType": "script",
+        "impliedStrict": true, 
+    },
+    "globals": {
         "MathJax": true,
         "ActiveXObject": true,
         "cookie": true,
     },
     "rules": {
-    	"quotes": ["off", "double", "avoid-escape"],
-    	"indent": ["off", 2],
-    	"no-console": "off",
-    	"no-useless-escape": "off",
-    	"no-empty": "off",
-    	"no-unused-vars": "off",
+        "quotes": ["off", "double", "avoid-escape"],
+        "indent": ["off", 2],
+        "no-console": "off",
+        "no-useless-escape": "off",
+        "no-empty": "off",
+        "no-unused-vars": "off",
 
-      "no-redeclare": "warn",
+        "no-redeclare": "warn",
     },
 };
